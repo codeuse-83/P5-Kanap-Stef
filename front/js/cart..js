a024c35db39300e277b51b9350efa4a9606cbe31
Stef <stephaniebinther@gmail.com>
2022-11-08 17:40:11 +0100

    Affichage des produits, envoie d'un message

--- front/js/cart..js
+++ front/js/cart..js
@@ -19,3 +19,31 @@ function nameOrder() {
 
 api_products(cart);
 
+//Si l'article n'est pas présent dans le localStorage, renvoie d'un message
+async function getAPIProducts(products) {
+    if (products === null || products == 0) {
+      document.querySelector("#totalQuantity").innerHTML = "0";
+      document.querySelector("#totalPrice").innerHTML = "0";
+      document.querySelector("h1").innerHTML =
+        "Vous n'avez pas d'article dans le panier";
+    }
+    try {
+      for (let i = 0; i < products.length; i++) {
+        let api_product = null;
+        await fetch(`http://localhost:3000/api/products/` + products[i].id)
+          .then((res) => res.json())
+          .then((data) => (api_product = data));
+        if (!api_product) {
+          continue;
+        }
+        api_product.color = products[i].color;
+        api_product.quantity = products[i].quantity;
+        api_products.push(api_product);
+        nameOrder();
+      }
+      displayProducts();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
