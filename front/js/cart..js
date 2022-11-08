8430a779b4505aa713572a0912614bcb6f01ed1d
Stef <stephaniebinther@gmail.com>
2022-11-08 14:52:04 +0100

    Affichage des produits

--- front/js/cart..js
+++ front/js/cart..js
@@ -1,2 +1,21 @@
+let cart = JSON.parse(localStorage.getItem("product_client"));
+let api_products = [];
 
+//Affichage des produits par ordre alphabétique
+  
+function nameOrder() {
+  api_products.sort(function (a, b) {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+}
+
+api_products(cart);
 
